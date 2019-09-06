f4e4cdbfc586c4223c8d37e79f7eb5c4523556b6
Raph Rusak <rrusak5@gmail.com>
2019-09-05 21:28:26 -0500

    GitDemo

---
+++ IT226Assignment1/src/edu/ilstu/GitDemo.java
@@ -0,0 +1,23 @@
+package edu.ilstu;
+
+/**
+ * Use this class for making multiple commits. For example,
+ * fill in each function and make a commit
+ * @author rsaripa
+ *
+ */
+
+public class GitDemo {
+
+	
+	
+	public double areaSquare(int a){
+		double area= a*a;
+		
+		return area; 
+	}
+	
+	/**
+	 * Please add any other two functions.
+	 */
+}
---
+++ IT226Assignment1/src/edu/ilstu/GitDemoTest.java
@@ -0,0 +1,19 @@
+package edu.ilstu;
+
+public class GitDemoTest {
+
+	public static void main(String[] args) {
+		
+		/** 
+		 * Test the GitDemo.java file in this class.
+		 * 
+		 * Create a GitDemo Class Object and test
+		 * Try to catch an exception in a different commit
+		 * 
+		 * For Branch,
+		 * Please add another Java file to demonstrating Branching
+		 */
+
+	}
+
+}
